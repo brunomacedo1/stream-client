830e978eb940b1e4dff25d32d85a88154f0177cd
Bruno <brunomacedo_31@hotmail.com>
2021-05-14 21:31:47 -0300

    filtro de stream

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -25,18 +25,20 @@ class StreamList extends Component{
 
   renderList() {
     return this.props.streams.map(stream => {
-      return (
-        <div className="item" key={stream.id}>
-          {this.renderAdmin(stream)}
-          <i className="large middle aligned icon camera" />
-          <div className="content">
-            <Link to={`/streams/${stream.id}`} className="header">
-              <strong>{stream.title}</strong>
-            </Link>
-            <div className="description">{stream.description}</div>
+      if(stream.userId === this.props.currentUserId){
+        return (
+          <div className="item" key={stream.id}>
+            {this.renderAdmin(stream)}
+            <i className="large middle aligned icon camera" />
+            <div className="content">
+              <Link to={`/streams/${stream.id}`} className="header">
+                <strong>{stream.title}</strong>
+              </Link>
+              <div className="description">{stream.description}</div>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     });
   }
 
