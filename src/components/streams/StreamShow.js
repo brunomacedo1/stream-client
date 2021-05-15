06e52580d18dbb1ff6b5982dab206310e0d9c984
Bruno <brunomacedo_31@hotmail.com>
2021-05-14 21:27:19 -0300

    criacao da stream com flv.js

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux'
 import { fetchStream } from '../../actions'
 class StreamShow extends Component{
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id)
+    const { id } = this.props.match.params
+    this.props.fetchStream(id)
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    const { id } = this.props.match.params
+
+    if(this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   renderStream() {
@@ -14,11 +46,17 @@ class StreamShow extends Component{
 
     return (
       <React.Fragment>
+        <video 
+          ref={this.videoRef} 
+          style={{ width: '100%'}} 
+          controls
+        />
         <h1>{stream.title}</h1>
         <h5>{stream.description}</h5>
       </React.Fragment>
-    )
+    );
   }
+
   render() {
     return (
       <div>{this.renderStream()}</div>
